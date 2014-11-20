fcfe1d4bae296df566486995d397cd29a68fce5c
Kevin Qian <yanxiang.qian@gmail.com>
2014-11-20 11:21:46 +0800

    add a file

--- DDS/simulating_fake_data.R
+++ DDS/simulating_fake_data.R
@@ -1,11 +1,16 @@
 # Simulating fake data
 x_1 <- rnorm(1000,5,7)
-hist(x_1, col="grey")
+x_2 <- rgamma(1000,2)
+
+#hist(x_1, col="grey")
 true_error <- rnorm(1000,0,2)
 true_beta_0 <- 1.1
 true_beta_1 <- -8.2
-y <- true_beta_0 + true_beta_1 * x_1 + true_error
+true_beta_2 <- 2.3
+y <- true_beta_0 + true_beta_1 * x_1 + true_beta_2 * x_2 + true_error
 hist(y)
-plot(x_1, y, pch=20, col="red")
+#plot(x_1, y, pch=20, col="red")
 
-model <- lm(y~x_1)
+m1 <- lm(y~x_1)
+
+
