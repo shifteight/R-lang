fea812209eac98bdf2470c0ef59d08dfd46971a8
Kevin Qian <yanxiang.qian@gmail.com>
2014-11-24 22:02:53 +0800

    project2

--- HandsOn/project2.R
+++ HandsOn/project2.R
@@ -1,7 +1,22 @@
 # make a deck
 deck <- read.csv('deck.csv', stringsAsFactors=FALSE)
 
+# deal
+deal <- function(cards) {
+    cards[1,]
+}
 
-random <- sample(1:52, size=52)
-deck4 <- deck[random,]
-head(deck4)
+# shuffle decks
+shuffle <- function(cards) {
+  random <- sample(1:52, size=52)
+  cards[random,]
+}
+
+show_env <- function() {
+    a <- 1
+    b <- 2
+    c <- 3
+    list(ran.in=environment(),
+         parent=parent.env(environment()),
+         objects=ls.str(environment()))
+}
