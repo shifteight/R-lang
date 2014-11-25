bc91eba7dca5b1cad86a4effce73b18f50710d3e
Kevin Qian <yanxiang.qian@gmail.com>
2014-11-25 17:50:18 +0800

    project 3

--- HandsOn/project2.R
+++ HandsOn/project2.R
@@ -1,7 +1,25 @@
 # make a deck
-deck <- read.csv('deck.csv', stringsAsFactors=FALSE)
+# deck <- read.csv('deck.csv', stringsAsFactors=FALSE)
 
+# A self-contained card game
 
-random <- sample(1:52, size=52)
-deck4 <- deck[random,]
-head(deck4)
+setup <- function(deck) {
+  DECK <- deck
+
+  DEAL <- function() {
+    card <- deck[1,]
+    assign('deck', deck[-1,], envir=parent.env(environment()))
+    card
+  }
+  
+  SHUFFLE <- function() {
+    random <- sample(1:52, size=52)
+    assign('deck', DECK[random,], envir=parent.env(environment()))
+  }
+
+  list(deal = DEAL, shuffle = SHUFFLE)
+}
+
+cards <- setup(deck)
+deal <- cards$deal
+shuffle <- cards$shuffle
