393c11b626e2b144b9062188bd814a88122ae3f8
Kevin Qian <yanxiang.qian@gmail.com>
2015-02-17 15:26:17 +0800

    nn

---
+++ MLwR/concrete.R
@@ -0,0 +1,28 @@
+concrete <- read.csv('concrete.csv')
+str(concrete)
+
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+concrete_norm <- as.data.frame(lapply(concrete, normalize))
+
+concrete_train <- concrete_norm[1:773, ]
+concrete_test <- concrete_norm[774:1030, ]
+
+require(neuralnet)
+concrete_model <- neuralnet(strength ~ cement + slag + ash + water
+                            + superplastic + coarseagg + fineagg + age,
+                            data=concrete_train)
+
+plot(concrete_model)
+
+model_results <- compute(concrete_model, concrete_test[1:8])
+predicted_strength <- model_results$net.result
+cor(predicted_strength, concrete_test$strength)
+
+concrete_model2 <- neuralnet(strength ~ cement + slag + ash + water
+                            + superplastic + coarseagg + fineagg + age,
+                            data=concrete_train, hidden=5)
+
+plot(concrete_model2)
