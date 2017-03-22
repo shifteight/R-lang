3ad1e1c743743ea4ab1028f5cb67b7393cea4b54
Kevin Qian <scifi.org@gmail.com>
2017-03-22 23:18:44 +0800

    R4DS

--- R4DS/demo_scripts.R
+++ R4DS/demo_scripts.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 ggplot(data=mpg) +
   geom_point(mapping = aes(x=displ,y=hwy))
 
@@ -41,14 +43,34 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_grid(drv ~ .)
 
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
 
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
   geom_point() + 
   geom_smooth()
 
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth(data = filter(mpg, class == "subcompact"), se = FALSE)
+
 ggplot(mapping = aes(x = displ, y = hwy)) + 
   geom_point(data = mpg) + 
   geom_smooth(data = mpg)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) + 
+  geom_point() + 
+  geom_smooth(se = FALSE)
 
 ggplot(data=diamonds) +
   geom_bar(mapping=aes(x=cut, fill=cut))
