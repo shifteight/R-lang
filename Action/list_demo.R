5e2ff3613a42db2a7aa70ca779c6e6414e684ff1
Kevin Qian <scifi.org@gmail.com>
2018-01-24 17:16:47 +0800

    HSAUR 3

---
+++ Action/list_demo.R
@@ -0,0 +1,7 @@
+g <- "My First List"
+h <- c(25, 25, 18, 39)
+j <- matrix(1:10, nrow=5)
+k <- c("one", "two", "three")
+mylist <- list(title=g, ages=h, j, k)
+mylist
+mylist[[2]]
--- HSAUR3/ch1/scripts.R
+++ HSAUR3/ch1/scripts.R
@@ -1,1 +1,20 @@
-data(Forbes2000)
+require(HSAUR3)
+str(Forbes2000)
+
+companies <- Forbes2000$name
+
+# the three companies with the lowest sales
+order_sales <- order(Forbes2000$sales)
+companies[order_sales[1:3]]
+
+# the three top sellers
+Forbes2000[order_sales[c(2000, 1999, 1998)],
+           c("name", "sales", "profits", "assets")]
+
+# the companies with assets of more than 1000 billion US dollars
+Forbes2000[Forbes2000$assets > 1000,
+           c("name", "sales", "profits", "assets")]
+
+# the observations with profit information missing
+na_profits <- is.na(Forbes2000$profits)
+Forbes2000[na_profits, c("name", "sales", "profits", "assets")]
